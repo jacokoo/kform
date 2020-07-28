fde21cf0dfb46c695e7eb567356a01c8a5d3332d
jacokoo <jaco.koo@guyong.in>
2020-07-28 18:35:21 +0800

    add default value support

--- src/main/kotlin/com/github/jacokoo/kform/Property.kt
+++ src/main/kotlin/com/github/jacokoo/kform/Property.kt
@@ -11,6 +11,7 @@ class FormProperty<T>(private val data: FormData, private val converter: Convert
         }
 
     fun required(): RequiredFormProperty<T> = RequiredFormProperty(data, converter)
+    fun default(d: T) = DefaultValueProperty(data, converter, d)
 }
 
 class RequiredFormProperty<T>(private val data: FormData, private val converter: Converter<T>): ReadOnlyProperty<KForm, T> {
@@ -19,4 +20,9 @@ class RequiredFormProperty<T>(private val data: FormData, private val converter:
             if (it.isNullOrBlank()) throw ViolationException("${property.name} is required")
             else converter.convert(property.name, it)
         }
+}
+
+class DefaultValueProperty<T>(private val data: FormData, private val converter: Converter<T>, private val default: T): ReadOnlyProperty<KForm, T> {
+    override fun getValue(thisRef: KForm, property: KProperty<*>): T =
+        data[property.name]?.let { converter.convert(property.name, it) } ?: default
 }
